165f3b8a4e056e78ed3d46d62b29826a3da62327
Charles Ewel and Joel Delight <charles+joel@>
2016-07-14 10:08:51 -0700

    add user select to form for writing reviews

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,112 +10,155 @@ namespace BestRestaurant
   {
     public HomeModule()
     {
-      // Get["/"]=_=>View["index.cshtml"];
-      //
-      // Delete["/"]=_=>{
-      //   Cuisine.DeleteAll();
-      //   Restaurant.DeleteAll();
-      //   return View["index.cshtml"];
-      // };
-      //
-      // Get["/cuisines"]=_=>{
-      //   List<Cuisine> allCuisines = Cuisine.GetAll();
-      //   return View["cuisines.cshtml", allCuisines];
-      // };
-      //
-      // Get["/cuisine/{id}"] = parameters => {
-      //   Dictionary<string, object> model = new Dictionary<string, object>();
-      //   var selectedCuisine = Cuisine.Find(parameters.id);
-      //   var cuisineRestaurants = selectedCuisine.GetRestaurants();
-      //   model.Add("cuisine", selectedCuisine);
-      //   model.Add("restaurants", cuisineRestaurants);
-      //   return View ["cuisine.cshtml", model];
-      // };
-      //
-      // Delete["/cuisine/deleted/{id}"]=parameters=>{
-      //   Cuisine selectedCuisine = Cuisine.Find(parameters.id);
-      //   selectedCuisine.Delete();
-      //   List<Cuisine> allCuisines = Cuisine.GetAll();
-      //   return View["cuisines.cshtml", allCuisines];
-      // };
-      //
-      // Get["/cuisine/new"]=_=>View["cuisine_new.cshtml"];
-      //
-      // Post["/cuisine/success"]=_=>{
-      //   Cuisine newCuisine = new Cuisine(Request.Form["cuisine-name"]);
-      //   newCuisine.Save();
-      //   List<Cuisine> allCuisines = Cuisine.GetAll();
-      //   return View["cuisines.cshtml", allCuisines];
-      // };
-      //
-      // Get["/restaurants"]=_=>{
-      //   List<Restaurant> allRestaurants = Restaurant.GetAll();
-      //   return View["restaurants.cshtml", allRestaurants];
-      // };
-      //
-      // Get["/restaurant/{id}"] = parameters => {
-      //   Dictionary<string, object> model = new Dictionary<string, object>();
-      //   var selectedRestaurant = Restaurant.Find(parameters.id);
-      //   var selectedReviews = selectedRestaurant.GetReviews();
-      //   model.Add("restaurant", selectedRestaurant);
-      //   model.Add("reviews", selectedReviews);
-      //   return View["restaurant.cshtml", model];
-      // };
-      //
-      // Get["/restaurant/edit/{id}"] = parameters => {
-      //   Dictionary<string, object> model = new Dictionary<string, object>();
-      //   var selectedRestaurant = Restaurant.Find(parameters.id);
-      //   var allCuisines = Cuisine.GetAll();
-      //   model.Add("restaurant", selectedRestaurant);
-      //   model.Add("cuisines", allCuisines);
-      //   return View["restaurant_edit.cshtml", model];
-      // };
-      //
-      // Delete["/restaurant/deleted/{id}"]=parameters=>{
-      //   Restaurant selectedRestaurant = Restaurant.Find(parameters.id);
-      //   selectedRestaurant.Delete();
-      //   List<Restaurant> allRestaurants = Restaurant.GetAll();
-      //   return View["restaurants.cshtml", allRestaurants];
-      // };
-      //
-      // Get["/restaurant/new"]=_=>{
-      //   List<Cuisine> allCuisines = Cuisine.GetAll();
-      //   return View["restaurant_new.cshtml", allCuisines];
-      // };
-      //
-      // Post["/restaurant/success"]=_=>{
-      //   Restaurant newRestaurant = new Restaurant(Request.Form["restaurant-name"], Request.Form["cuisine-id"]);
-      //   newRestaurant.Save();
-      //   List<Restaurant> allRestaurants = Restaurant.GetAll();
-      //   return View["restaurants.cshtml", allRestaurants];
-      // };
-      //
-      // Patch["/restaurant/success/{id}"]=parameters=>{
-      //   var selectedRestaurant = Restaurant.Find(Request.Form["restaurant-id"]);
-      //   selectedRestaurant.SetName(Request.Form["restaurant-name"]);
-      //   selectedRestaurant.SetCuisineId(Request.Form["cuisine-id"]);
-      //   Dictionary<string, object> model = new Dictionary<string, object>();
-      //   var selectedReviews = selectedRestaurant.GetReviews();
-      //   model.Add("restaurant", selectedRestaurant);
-      //   model.Add("reviews", selectedReviews);
-      //   return View["restaurant.cshtml", model];
-      // };
-      //
-      // Get["/review/new/{id}"]= parameters =>{
-      //   Restaurant restaurantToBeReviewed = Restaurant.Find(parameters.id);
-      //   return View["review_new.cshtml", restaurantToBeReviewed];
-      // };
-      //
-      // Post["/review/success"]=_=>{
-      //   Review newReview = new Review(Request.Form["review-content"], Request.Form["restaurant-id"]);
-      //   newReview.Save();
-      //   Dictionary<string, object> model = new Dictionary<string, object>();
-      //   var selectedRestaurant = Restaurant.Find(Request.Form["restaurant-id"]);
-      //   var selectedReviews = selectedRestaurant.GetReviews();
-      //   model.Add("restaurant", selectedRestaurant);
-      //   model.Add("reviews", selectedReviews);
-      //   return View["restaurant.cshtml", model];
-      // };
+      Get["/"]=_=>{
+        if (User.GetAll().Count == 0)
+        {
+          User admin = new User("Admin", 1);
+          admin.Save();
+          User guest = new User("Guest", 1);
+          guest.Save();
+        }
+      return View["index.cshtml"];
+    };
+
+      Delete["/"]=_=>{
+        Cuisine.DeleteAll();
+        Restaurant.DeleteAll();
+        return View["index.cshtml"];
+      };
+
+      Get["/cuisines"]=_=>{
+        List<Cuisine> allCuisines = Cuisine.GetAll();
+        return View["cuisines.cshtml", allCuisines];
+      };
+
+      Get["/cuisine/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var selectedCuisine = Cuisine.Find(parameters.id);
+        var cuisineRestaurants = selectedCuisine.GetRestaurants();
+        model.Add("cuisine", selectedCuisine);
+        model.Add("restaurants", cuisineRestaurants);
+        return View ["cuisine.cshtml", model];
+      };
+
+      Delete["/cuisine/deleted/{id}"]=parameters=>{
+        Cuisine selectedCuisine = Cuisine.Find(parameters.id);
+        selectedCuisine.Delete();
+        List<Cuisine> allCuisines = Cuisine.GetAll();
+        return View["cuisines.cshtml", allCuisines];
+      };
+
+      Get["/cuisine/new"]=_=>View["cuisine_new.cshtml"];
+
+      Post["/cuisine/success"]=_=>{
+        Cuisine newCuisine = new Cuisine(Request.Form["cuisine-name"]);
+        newCuisine.Save();
+        List<Cuisine> allCuisines = Cuisine.GetAll();
+        return View["cuisines.cshtml", allCuisines];
+      };
+
+      Get["/restaurants"]=_=>{
+        List<Restaurant> allRestaurants = Restaurant.GetAll();
+        return View["restaurants.cshtml", allRestaurants];
+      };
+
+      Get["/restaurant/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var selectedRestaurant = Restaurant.Find(parameters.id);
+        var selectedReviews = selectedRestaurant.GetReviews();
+        var allUsers = User.GetAll();
+        Dictionary<int, string> usernames = new Dictionary<int, string>{};
+        foreach(User reviewer in allUsers)
+        {
+          usernames.Add(reviewer.GetId(), reviewer.GetName());
+        }
+        model.Add("users", usernames);
+        model.Add("restaurant", selectedRestaurant);
+        model.Add("reviews", selectedReviews);
+        return View["restaurant.cshtml", model];
+      };
+
+      Get["/restaurant/edit/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var selectedRestaurant = Restaurant.Find(parameters.id);
+        var allCuisines = Cuisine.GetAll();
+        model.Add("restaurant", selectedRestaurant);
+        model.Add("cuisines", allCuisines);
+        return View["restaurant_edit.cshtml", model];
+      };
+
+      Delete["/restaurant/deleted/{id}"]=parameters=>{
+        Restaurant selectedRestaurant = Restaurant.Find(parameters.id);
+        selectedRestaurant.Delete();
+        List<Restaurant> allRestaurants = Restaurant.GetAll();
+        return View["restaurants.cshtml", allRestaurants];
+      };
+
+      Get["/restaurant/new"]=_=>{
+        List<Cuisine> allCuisines = Cuisine.GetAll();
+        return View["restaurant_new.cshtml", allCuisines];
+      };
+
+      Post["/restaurant/success"]=_=>{
+        Restaurant newRestaurant = new Restaurant(Request.Form["restaurant-name"], Request.Form["cuisine-id"]);
+        newRestaurant.Save();
+        List<Restaurant> allRestaurants = Restaurant.GetAll();
+        return View["restaurants.cshtml", allRestaurants];
+      };
+
+      Patch["/restaurant/success/{id}"]=parameters=>{
+        var selectedRestaurant = Restaurant.Find(Request.Form["restaurant-id"]);
+        selectedRestaurant.SetName(Request.Form["restaurant-name"]);
+        selectedRestaurant.SetCuisineId(Request.Form["cuisine-id"]);
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var selectedReviews = selectedRestaurant.GetReviews();
+        var allUsers = User.GetAll();
+        Dictionary<int, string> usernames = new Dictionary<int, string>{};
+        foreach(User reviewer in allUsers)
+        {
+          usernames.Add(reviewer.GetId(), reviewer.GetName());
+        }
+        model.Add("users", usernames);
+        model.Add("restaurant", selectedRestaurant);
+        model.Add("reviews", selectedReviews);
+        return View["restaurant.cshtml", model];
+      };
+
+      Get["/review/new/{id}"]= parameters =>{
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var allUsers = User.GetAll();
+        var restaurantToBeReviewed = Restaurant.Find(parameters.id);
+        model.Add("users", allUsers);
+        model.Add("restaurant", restaurantToBeReviewed);
+        return View["review_new.cshtml", model];
+      };
+
+      Post["/review/success"]=_=>{
+        Review newReview = new Review(Request.Form["review-content"], Request.Form["restaurant-id"], Request.Form["user-id"]);
+        newReview.Save();
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var selectedRestaurant = Restaurant.Find(Request.Form["restaurant-id"]);
+        var selectedReviews = selectedRestaurant.GetReviews();
+        var allUsers = User.GetAll();
+        Dictionary<int, string> usernames = new Dictionary<int, string>{};
+        foreach(User reviewer in allUsers)
+        {
+          usernames.Add(reviewer.GetId(), reviewer.GetName());
+        }
+        model.Add("users", usernames);
+        model.Add("restaurant", selectedRestaurant);
+        model.Add("reviews", selectedReviews);
+        return View["restaurant.cshtml", model];
+      };
+
+      Get["/user/new"]=_=>View["user_new.cshtml"];
+
+      Post["/user/success"]=_=>{
+        User newUser = new User(Request.Form["user-name"], Request.Form["display-preference"]);
+        newUser.Save();
+        return View["index.cshtml"];
+      };
+
     }
   }
 }
